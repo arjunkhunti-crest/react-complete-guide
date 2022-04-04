31617a7b5701ce46498c22df273d589ee65c771a
akhunti <akhunti@splunk.com>
2022-04-04 14:54:07 +0530

    init commit for react-complete-guide

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  // To add some tag in regular js file
+
+  // const para = document.createElement("p");
+  // para.textContent = "This is paragraph created with regular js";
+  // document.getElementById("root").append(para);
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 3, 12),
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 899.99,
+      date: new Date(2021, 10, 17),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.43,
+      date: new Date(2021, 3, 30),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 7, 25),
+    },
+  ];
+
+  //  return React.createElement(
+  //   "div",
+  //   null,
+  //   React.createElement("h2", null, "Let's get started!"),
+  //   React.createElement(Expenses, {expenses: expenses})
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
