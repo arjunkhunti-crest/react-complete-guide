b3436eb46de56a1aa0338b061169265dd1fb5e55
akhunti <akhunti@splunk.com>
2022-04-05 10:51:21 +0530

    Added React StrictMode

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 import "./index.css";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import React from "react";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // ReactDOM.render(<App />, document.getElementById("root"));
